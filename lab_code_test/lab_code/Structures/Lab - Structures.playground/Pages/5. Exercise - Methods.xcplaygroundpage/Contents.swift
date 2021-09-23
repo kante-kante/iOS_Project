2757f90fc21edaae259933aa5d8d3245d4a66019
kang <micro.kante@gmail.com>
2021-09-23 20:25:03 +0900

    init

---
+++ lab_code_test/lab_code/Structures/Lab - Structures.playground/Pages/5. Exercise - Methods.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+/*:
+## Exercise - Methods
+ 
+ A `Book` struct has been created for you below. Add an instance method on `Book` called `description` that will print out facts about the book. Then create an instance of `Book` and call this method on that instance.
+ */
+struct Book {
+    var title: String
+    var author: String
+    var pages: Int
+    var price: Double
+        
+    func description() {
+        print("This is \(title) by \(author). pages \(pages), and price \(price)$")
+    }
+}
+
+var book = Book(title: "Harry Potter", author: "J.K. Rowling", pages: 400, price: 30)
+book.description()
+//:  A `Post` struct has been created for you below, representing a generic social media post. Add a mutating method on `Post` called `like` that will increment `likes` by one. Then create an instance of `Post` and call `like()` on it. Print out the `likes` property before and after calling the method to see whether or not the value was incremented.
+struct Post {
+    var message: String
+    var likes: Int
+    var numberOfComments: Int
+
+    mutating func like() {
+        self.likes += 1
+    }
+}
+
+var post = Post(message: "message", likes: 0, numberOfComments: 0)
+post.like()
+print(post.likes)
+/*:
+[Previous](@previous)  |  page 5 of 10  |  [Next: App Exercise - Workout Functions](@next)
+ */
